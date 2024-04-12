9653af3485e641bb1a384df8cba357fc4f10d7a2
hjy <184638634@qq.com>
2024-04-12 16:01:49 +0800

    提交new页面

---
+++ src/views/new.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  components: {},
+  watch: {},
+  computed: {},
+  beforeCreate() {},
+  mounted() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+};
+</script>
+
+<style scoped></style>
